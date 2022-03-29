ec55b00edadb401c5381924d57eac16c544529e3
TruthTheDeveloper <henrytruthidongesit@gmail.com>
2022-03-28 22:27:22 -0700

    28th march 2022

---
+++ components/page/LandingPage.js
@@ -0,0 +1,7 @@
+import Header from './landingPage/Header';
+const LandingPage = () => {
+     return(
+         <Header/>
+     )
+}
+export default LandingPage;
---
+++ components/page/landingPage/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return(
+        <section id="header">
+            <div>
+                <h1>This is the Header</h1>
+            </div>
+        </section>
+    )
+}
+
+export default Header;
