aacabb065e90cc45cfc2aeb4787199030d611008
TruthTheDeveloper <henrytruthidongesit@gmail.com>
2022-03-28 23:09:58 -0700

    28th march 2022

---
+++ components/layout/Layout.js
@@ -0,0 +1,12 @@
+import Head from "next/head"
+
+const Layout = () => {
+    return <>
+        <Head>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link  href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet"/>
+        </Head>
+    </>
+}
+export default Layout
--- components/page/landingPage/Header.js
+++ components/page/landingPage/Header.js
@@ -1,8 +1,16 @@
+import Image from 'next/image';
+import amazed from '../../../images/amazed.png'
+
 const Header = () => {
     return(
-        <section id="header">
-            <div>
-                <h1 className="text-red-400">This is the Header</h1>
+        <section id="header" className="" style={{backgroundColor:'#3C8224'}}>
+            <div className="flex">
+                <div className="basis-1/2">
+                    <h1 className="text-white text-4xl text-center font-medium">Over 2000 jobs available for grabs</h1>
+                </div>
+                <div className="">
+                        <Image src={amazed}  alt="text" width={550} height={400}/>
+                </div>
             </div>
         </section>
     )
