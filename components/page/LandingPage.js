db27356c9f2cef6ddfbe6e1151aafba307564743
TruthTheDeveloper <henrytruthidongesit@gmail.com>
2022-03-29 00:43:39 -0700

    28th march 2022

--- components/page/LandingPage.js
+++ components/page/LandingPage.js
@@ -1,7 +1,42 @@
 import Header from './landingPage/Header';
+import Main from './landingPage/Main';
+import React, {useState} from 'react'
+import Lottie from 'react-lottie';
+import * as animationData from './pinjump.json';
+
 const LandingPage = () => {
+
+    const[isStopped, setIsStopped] = useState(false)
+    const[isPaused, setIsPaused] = useState(false)
+
+    const buttonStyle = {
+        display: 'block',
+        margin: '10px auto'
+      };
+
+      const defaultOptions = {
+        loop: true,
+        autoplay: true, 
+        animationData: animationData,
+        rendererSettings: {
+          preserveAspectRatio: 'xMidYMid slice'
+        }
+      };
+   
      return(
-         <Header/>
+         <>
+            <Header/>
+            <Main/>
+
+            <Lottie options={defaultOptions}
+              height={400}
+              width={400}
+              isStopped={this.state.isStopped}
+              isPaused={this.state.isPaused}/>
+      <button style={buttonStyle} onClick={() => setIsStopped(true)}>stop</button>
+      <button style={buttonStyle} onClick={() => setIsStopped(false)}>play</button>
+      <button style={buttonStyle} onClick={() => setIsPaused(prev => !prev)}>pause</button>
+         </>
      )
 }
 export default LandingPage;
---
+++ components/page/landingPage/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Lottie from 'react-lottie';
+import * as animationData from './pinjump.json'
+const Main = () => {
+
+    return(
+        <section>
+
+        </section>
+    )
+}
+export default Main
