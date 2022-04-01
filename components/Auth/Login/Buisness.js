e483ab0b986332ae9cec83cfeab8214213cda747
TruthTheDeveloper <henrytruthidongesit@gmail.com>
2022-03-31 20:05:04 -0700

    31 march 2022

--- components/Auth/Login/Buisness.js
+++ components/Auth/Login/Buisness.js
@@ -40,7 +40,7 @@ const Buisness = () => {
                 </div>
                 <button onClick={submitHandler} className="w-48 mx-auto h-12 text-white rounded-full my-6 text-lg" style={{backgroundColor:"#14A800"}}>Log In</button>
                     <div className="flex">
-                        <h1 className="py-2 pr-1 " style={{color:"#14A800"}}>Dont't have an account ?</h1>
+                        <h1 className="py-2 pr-1 " style={{color:"#14A800"}}>Dont have an account ?</h1>
                         <Link href="/buisness">
                             <span className="py-2 underline cursor-pointer" style={{color:"#14A800"}}>Sign Up</span>
                         </Link>
