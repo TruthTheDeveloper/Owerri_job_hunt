bddaf28ce13a9781dd371c9902430b25453b16cf
TruthTheDeveloper <henrytruthidongesit@gmail.com>
2022-04-02 10:16:43 -0700

    added carousel sider

--- components/page/landingPage/Talents.js
+++ components/page/landingPage/Talents.js
@@ -1,16 +1,28 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-// import { Pagination } from "swiper";
-// import 'swiper/css';
+import Alex from '../../../images/talents/Alex.jpg';
+import Edgar from '../../../images/talents/Edgar.jpg';
+import ikeagwu from '../../../images/talents/ikeagwu.jpg';
+import jane from '../../../images/talents/jane.jpg';
+import marvis from '../../../images/talents/marvis.jpg';
+import vitalis from '../../../images/talents/vitalis.jpg'
+import Image from 'next/image';
+import { Pagination } from "swiper";
+import 'swiper/css';
+
+// Alexis Munonozi
+// Edgar Ampiire
+// Ikeagwu Ebuka
+// Ikeagwu Ebuka
 
 const Talent = () => {
     
 
     return (
-        <>
-        
-        <h1 c1assName="text-red-200">A glimpse of few Owerri Talents</h1>
+        <section className="py-24">
 
-            {/* <Swiper
+        <h1 className="font-semibold text-center py-16 text-2xl">A GLIMPSE OF FEW OWERRI TALENTS</h1>
+
+            <Swiper
         slidesPerView={3}
         spaceBetween={30}
         pagination={{
@@ -19,18 +31,58 @@ const Talent = () => {
         modules={[Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide></SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper> */}
+        <SwiperSlide>
+          <div className="bg-white shadow-lg border flex flex-col mx-12 px-8 pt-4">
 
-        </>
+              <Image src={Alex} height={250} width={300}/>
+              <h1 className="text-lg font-semibold mt-4">Alex Munonozi</h1>
+              <p className="text-center text-xl my-4">Digital marketer</p>
+              <button className="w-48 mx-auto h-12 text-white rounded-lg my-6 text-lg" style={{backgroundColor:"#14A800"}}>View Profile</button>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+            <div className="bg-white shadow-lg border flex flex-col mx-12 px-8 pt-4 ">
+              <Image src={Edgar} height={250} width={200}/>
+              <h1 className="text-lg font-semibold mt-4">Alex Munonozi</h1>
+              <p className="text-center text-xl my-4">Digital marketer</p>
+              <button className="w-48 mx-auto h-12 text-white rounded-lg my-6 text-lg" style={{backgroundColor:"#14A800"}}>View Profile</button>
+            </div>
+          </SwiperSlide>
+        <SwiperSlide>
+            <div className="bg-white shadow-lg border flex flex-col mx-12 px-8 pt-4">
+              <Image src={ikeagwu} height={250} width={200}/>
+              <h1 className="text-lg font-semibold mt-4">Alex Munonozi</h1>
+              <p className="text-center text-xl my-4">Digital marketer</p>
+              <button className="w-48 mx-auto h-12 text-white rounded-lg my-6 text-lg" style={{backgroundColor:"#14A800"}}>View Profile</button>
+            </div>
+          </SwiperSlide>
+        <SwiperSlide>
+          <div className="bg-white shadow-lg border flex flex-col mx-12 px-8 pt-4">
+            <Image src={jane} height={250} width={200}/>
+            <h1 className="text-lg font-semibold mt-4">Alex Munonozi</h1>
+              <p className="text-center text-xl my-4">Digital marketer</p>
+              <button className="w-48 mx-auto h-12 text-white rounded-lg my-6 text-lg" style={{backgroundColor:"#14A800"}}>View Profile</button>
+          </div>
+          </SwiperSlide>
+        <SwiperSlide>
+          <div className="bg-white shadow-lg border flex flex-col mx-12 px-8 pt-4">
+            <Image src={marvis} height={250} width={200}/>
+            <h1 className="text-lg font-semibold mt-4">Alex Munonozi</h1>
+              <p className="text-center text-xl my-4">Digital marketer</p>
+              <button className="w-48 mx-auto h-12 text-white rounded-lg my-6 text-lg" style={{backgroundColor:"#14A800"}}>View Profile</button>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="bg-white shadow-lg border flex flex-col mx-12 px-8 pt-4">
+            <Image src={vitalis} height={250} width={200}/>
+            <h1 className="text-lg font-semibold mt-4">Alex Munonozi</h1>
+              <p className="text-center text-xl my-4">Digital marketer</p>
+              <button className="w-48 mx-auto h-12 text-white rounded-lg my-6 text-lg" style={{backgroundColor:"#14A800"}}>View Profile</button>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+
+        </section>
     )
 }
 
