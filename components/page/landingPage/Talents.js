7c21246a8e69209321e48ebb172b599d5b761a0d
TruthTheDeveloper <henrytruthidongesit@gmail.com>
2022-04-02 02:47:32 -0700

    installed swiper

--- components/page/landingPage/Talents.js
+++ components/page/landingPage/Talents.js
@@ -1,19 +1,36 @@
-import Carousel from 'carousel-react-rcdev';
-const Talent = () => {
+import { Swiper, SwiperSlide } from "swiper/react";
+// import { Pagination } from "swiper";
+// import 'swiper/css';
 
+const Talent = () => {
+    
 
     return (
-        <section>
-            <h1>A glimpse of few Owerri Talents</h1>
-            <Carousel>
-            <img src='https://via.placeholder.com/150' alt='imagem' title='imagem' />
-            <img src='https://via.placeholder.com/150' alt='imagem' title='imagem' />
-            <img src='https://via.placeholder.com/150' alt='imagem' title='imagem' />
-            <img src='https://via.placeholder.com/150' alt='imagem' title='imagem' />
-            <img src='https://via.placeholder.com/150' alt='imagem' title='imagem' />
-        </Carousel>
+        <>
+        
+        <h1 c1assName="text-red-200">A glimpse of few Owerri Talents</h1>
 
-        </section>
+            {/* <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide></SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper> */}
+
+        </>
     )
 }
 
